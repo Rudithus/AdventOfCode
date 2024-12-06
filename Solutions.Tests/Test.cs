0b3e44294112d2f3d237c58d95825763681b642a
Rudithus <cagri.ozer@outlook.com>
2024-12-06 02:32:43 +0100

    Day05

--- Solutions.Tests/Test.cs
+++ Solutions.Tests/Test.cs
@@ -9,6 +9,7 @@ namespace Solutions.Tests
         private readonly ITestOutputHelper _testOutputHelper = testOutputHelper;
 
         [Theory]
+        [InlineData(typeof(Day05PrintQueue.Solution), 143)]
         [InlineData(typeof(Day04CeresSearch.Solution), 18)]
         [InlineData(typeof(Day03MullItOver.Solution), 161)]
         [InlineData(typeof(Day02RedNosedReports.Solution), 2)]
@@ -29,6 +30,7 @@ namespace Solutions.Tests
         }
 
         [Theory]
+        [InlineData(typeof(Day05PrintQueue.Solution), 123)]
         [InlineData(typeof(Day04CeresSearch.Solution), 9)]
         [InlineData(typeof(Day03MullItOver.Solution), 48)]
         [InlineData(typeof(Day02RedNosedReports.Solution), 8)]
